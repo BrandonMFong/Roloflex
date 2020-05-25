af19f6334bf5d5db2ce2b2702a5b802425707f1f
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2020-05-25 00:53:09 -0700

    work on inserting into database

--- scripts/ImportContacts.ps1
+++ scripts/ImportContacts.ps1
@@ -1,25 +1,23 @@
+# https://vwiki.co.uk/MySQL_and_PowerShell
+
 $null = [System.Reflection.Assembly]::LoadWithPartialName('MySql.Data');
 $myconnection = New-Object MySql.Data.MySqlClient.MySqlConnection
 $myconnection.ConnectionString = "server=localhost;user id=root;password=;database=roloflex;pooling=false";
 $myconnection.Open()
-$mycommand = New-Object MySql.Data.MySqlClient.MySqlCommand
-$mycommand.Connection = $myconnection
-
+$mycommand = $myconnection.CreateCommand();
 
 $CSVFile = Import-Csv $PSScriptRoot\..\imports\Export5132020.csv;
-foreach($row in $CSVFile)
+# foreach($row in $CSVFile)
+for([int]$i = 0;$i -lt $CSVFile.Length;$i++)
 {
-    $querystring =
-    "
-        set @val = (SELECT EXISTS (SELECT 1 FROM contacts));
-        set @id = (SELECT IF(@val=0, 1, (select id+1 from contacts)));
-        insert into contacts values (@id, uuid(), '$($row.FirstName)', '$($row.LastName)', '$($row.Email)', '$($row.Phone)');
-    ";
+    [string]$querystring = "insert into contacts values ((SELECT IF(@val=0, 1, (select id+1 from contacts))), uuid(), '$($CSVFile[$i].FirstName)', '$($CSVFile[$i].LastName)', '$($CSVFile[$i].Email)', '$($CSVFile[$i].Phone)');";
+    
     $mycommand.CommandText = $querystring;
+    $mycommand.Parameters.AddWithValue("@name", "(SELECT EXISTS (SELECT 1 FROM contacts))");
     # something is probably wrong with the query I am passing
     # the query works from the gui but not through here
-    $myreader = $mycommand.ExecuteReader();
-    while($myreader.Read()){ $myreader.GetString(0); }
+    $mycommand.ExecuteNonQuery(); # Execute command
+    $mycommand.Dispose()       
 }
 
 $myconnection.Close()
